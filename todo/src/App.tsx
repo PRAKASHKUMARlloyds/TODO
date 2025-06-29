d5e2a9facd31ce92500f8b29054ecfeef859108a
PRAKASHKUMARlloyds <pkumar91dst@gmail.com>
2025-06-29 06:18:03 +0000

    inital commit

---
+++ todo/src/App.tsx
@@ -0,0 +1,27 @@
+// App.tsx
+import React from 'react';
+import './App.css';
+const App: React.FC = () => {
+  const a =10;
+  console.log('hello')
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
